66f4f226fe2d299a82c3f7557e21754df19ef69f
emoltz <es5541@nyu.edu>
2023-01-25 15:18:41 -0500

    magic formula!

--- EthanOH/Jan25.cpp
+++ EthanOH/Jan25.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
 
+using namespace std;
 
 int main() {
     double pi = M_PI;
-
 
     cout.setf(ios::fixed);
     cout.setf(ios::showpoint);
     //the number in the parens below can set the precision of the decimal points.
     cout.precision(2);
 
-    cout << pi;
 
+    cout << pi;
 
     return 0;
 }
