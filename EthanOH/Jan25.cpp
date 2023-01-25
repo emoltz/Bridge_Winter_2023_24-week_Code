bb4b644af26227441227b0d44ad0f401c8b604f9
emoltz <es5541@nyu.edu>
2023-01-25 15:04:14 -0500

    magic formula!

--- EthanOH/Jan25.cpp
+++ EthanOH/Jan25.cpp
@@ -1,3 +1,16 @@
-//
-// Created by Ethan Shafran Moltz on 1/25/23.
-//
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+
+
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    //the number in the parens below can set the precision of the decimal points.
+    cout.precision(1);
+
+
+    return 0;
+}
