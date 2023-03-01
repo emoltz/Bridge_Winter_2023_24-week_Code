dac48daaeb65c8232e7a4cdb5d25e9b0acc9d1d7
emoltz <es5541@nyu.edu>
2023-03-01 15:28:33 -0500

    march1

--- EthanOH/March1.cpp
+++ EthanOH/March1.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main(){
-
+    string s = "1234_5";
+    char arr[5] = {'1', '2', '3', '4', '5'};
+    int intArr[5] = {1, 2, 3, 4, 5};
+    cout << (int)'_';
 
     return 0;
 }
